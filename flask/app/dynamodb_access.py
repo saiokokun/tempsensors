61841074f4836fec5dd837959a246632037636d9
Grigory Backman <90825412+saiokokun@users.noreply.github.com>
2022-02-22 11:43:44 +0100

    Add files via upload

---
+++ flask/app/dynamodb_access.py
@@ -0,0 +1,16 @@
+import os
+
+import boto3
+
+def get_resource():
+    return boto3.resource("dynamodb",
+                          aws_access_key_id=os.environ["AWS_KEY"],
+                          aws_secret_access_key=os.environ["AWS_SECRET_KEY"],
+                          region_name="us-east-1")
+
+
+def get_all_readings():
+    client = get_resource()
+    table = client.Table("temp_readings")
+    response = table.scan()
+    return response['Items']
